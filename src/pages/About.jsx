570ca7ff56c8748e18d29856d289fd36f2c3b9b1
Diana Akhmadova <dianaakhmadova@gmail.com>
2024-01-29 11:42:20 +0100

    add component collapse in About

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,16 +1,21 @@
-import React from 'react';
+import React from 'react'
 import Header from '../components/Header'
 import Banner from '../components/Banner'
 import imgBannerAbout from '../assets/mountains.png'
-//import Collapse from '../components/Collapse'
+import Collapse from '../components/Collapse'
+import aboutData from '../data/aboutData.json'
 import Footer from '../components/Footer'
 
 const About = () => {
     return (
         <div>
             <Header />
-            <Banner imageUrl={imgBannerAbout}/>
-          
+            <Banner imageUrl={imgBannerAbout} />
+            <main className='main_about'>
+                {aboutData.map((data) => (
+                    <Collapse key={data.id} title={data.title} content={data.description} />
+                ))}
+            </main>
             <Footer />
         </div>
     );

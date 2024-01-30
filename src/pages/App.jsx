1b3db8195d56b6fed2fa409c6f9e69288c05bce9
Diana Akhmadova <dianaakhmadova@gmail.com>
2024-01-30 14:53:28 +0100

    ajout route 404

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -3,19 +3,19 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import Housing from './Housing';
 import About from './About';
+import Page404 from '../pages/404';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/Housing' element={<Housing />} />
-        <Route path='/About' element={<About />} />
+        <Route path='/housing/:id' element={<Housing />} />
+        <Route path='/about' element={<About />} />
+        <Route path='*' element={<Page404 />} />
       </Routes>
     </BrowserRouter>
   )
 };
 
 export default App;
-
-

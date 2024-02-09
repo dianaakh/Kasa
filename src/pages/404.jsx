032a103f5279b61ec360b5ea80ef482414786e28
Diana Akhmadova <dianaakhmadova@gmail.com>
2024-02-09 15:11:32 +0100

    deplace header et footer dans app.jsx

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,12 +1,8 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
 
 const Page404 = () => {
   return (
-    <div>
-      <Header />
       <main className='main_error'>
         <h1>404</h1>
         <p className='text_error'>Oups! La page que vous demandez n'existe pas.</p>
@@ -14,8 +10,6 @@ const Page404 = () => {
           <p>Retourner sur la page d’accueil</p>
         </NavLink>
       </main>
-      <Footer />
-    </div>
   );
 };
 
